e01e9ea12f2d3dba0fd25fb5d54a0177a1d3e66c
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-06 00:10:45 +0800

    MyPortfolio.jsx

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,7 +1,7 @@
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
-import MyPortfolio from "../MyPortfolio"
+import MyPortfolio from "../MyPortfolio";
 
 export default function Home (){
     return (
