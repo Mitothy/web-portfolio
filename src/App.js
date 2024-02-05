737318432e754e777fc415fcd51ba29927a851e9
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-05 14:58:30 +0800

    Containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import react from 'react';
+import { BrowserRouter as Router, RouterProvider, Routes} from 'react-router-dom'; 
+import Home from './Pages/Home/Homescreen';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
