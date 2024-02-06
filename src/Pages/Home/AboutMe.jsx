9a150006fe45a724af3661db01e058170e18bbba
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-06 13:57:05 +0800

    Color Scheme Changes

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,20 +1,20 @@
-export default function AboutMe(){
+export default function AboutMe() {
     return (
         <section id="AboutMe" className="about--section">
-        <div className="about--section--img">
-            <img src="./img/about-me.png" alt="About Me"/>
-        </div>
-        <div className="hero--section--content--box--about--section--box">
-            <div className="hero--section-content">
-                <p className="section--title">About</p>
-                <h1 className="skills--section--heading"></h1>
-                <p className="hero--section--description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, quas. Amet soluta assumenda cum? </p>
-                
-                <p className="hero--section--description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, quas. Amet soluta assumenda cum? </p>
-
-                <p className="hero--section--description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, quas. Amet soluta assumenda cum? </p>
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me" />
             </div>
-        </div>
+            <div className="hero--section--content--box--about--section--box">
+                <div className="hero--section-content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills--section--heading"></h1>
+                    <p className="hero--section--description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, quas. Amet soluta assumenda cum? </p>
+
+                    <p className="hero--section--description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, quas. Amet soluta assumenda cum? </p>
+
+                    <p className="hero--section--description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum, quas. Amet soluta assumenda cum? </p>
+                </div>
+            </div>
         </section>
     )
 }
