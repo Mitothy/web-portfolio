42abfefdd00797f1b3112b0e91164581b22fed7b
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-06 10:31:31 +0800

    Done

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -2,7 +2,10 @@ import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import AboutMe from "../AboutMe";
 import MyPortfolio from "../MyPortfolio";
-import Testimonial from "../Testimonials"
+import Testimonial from "../Testimonials";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
+
 
 export default function Home (){
     return (
@@ -12,6 +15,8 @@ export default function Home (){
             <AboutMe />
             <MyPortfolio />
             <Testimonial />
+            <ContactMe />
+            <Footer />
         </>
     )
 }
