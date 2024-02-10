211bbb7b3ce8f168929baf17cc0fd58d36f93b75
Timothy Tan <98691592+Mitothy@users.noreply.github.com>
2024-02-10 11:47:16 +0800

    Fixing Experiences.jsx

--- src/Pages/Home/Experiences.jsx
+++ src/Pages/Home/Experiences.jsx
@@ -4,15 +4,16 @@ export default function Experiences() {
     return (
         <section className="skills--section" id="mySkills">
             <div className="portfolio--container">
-                <p clssName="section--title">About Me</p>
+                <p className="section--title">About Me</p>
                 <h2 className="skills-section--heading">Experiences</h2>
             </div>
             <div className="skills--section--container">
-                {data?.skills?.map((item, index) => (
+                {data?.experiences?.map((item, index) => (
                     <div key={index} className="skills--section--card">
 
                         <div className="skills--section--card--content">
                             <h3 className="skills--section--title">{item.title}</h3>
+                            <h2 className="skills--section--subtitle">{item.subtitle}</h2>
                             <p className="skills--section--description">{item.description}</p>
                         </div>
                         <div className="skills-section--img">
